71098fc5ea50cc4ed5b01e8d8f8f5d8dbf2f3144
Samuel Tian <swtian@mit.edu>
2023-10-15 23:15:33 -0400

    finish exercise 5

--- godb/order_by_op.go
+++ godb/order_by_op.go
@@ -1,10 +1,15 @@
 package godb
 
+import (
+    "sort"
+)
+
 // TODO: some code goes here
 type OrderBy struct {
 	orderBy []Expr // OrderBy should include these two fields (used by parser)
 	child   Operator
 	//add additional fields here
+    ascending []bool
 }
 
 // Order by constructor -- should save the list of field, child, and ascending
@@ -14,13 +19,15 @@ type OrderBy struct {
 // list should be in ascending (true) or descending (false) order.
 func NewOrderBy(orderByFields []Expr, child Operator, ascending []bool) (*OrderBy, error) {
 	// TODO: some code goes here
-	return nil, nil //replace me
-
+    if len(orderByFields) != len(ascending) {
+        return nil, GoDBError{MalformedDataError, "field lengths not equal"}
+    }
+    return &OrderBy{orderByFields, child, ascending}, nil
 }
 
 func (o *OrderBy) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+    return o.child.Descriptor()
 }
 
 // Return a function that iterators through the results of the child iterator in
@@ -36,5 +43,51 @@ func (o *OrderBy) Descriptor() *TupleDesc {
 // example, example of SortMultiKeys, and documentation at: https://pkg.go.dev/sort
 func (o *OrderBy) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, nil //replace me
+    childIter, err := o.child.Iterator(tid)
+    if err != nil {
+        return nil, err
+    }
+    var tuples []*Tuple
+    for t, err := childIter(); t != nil || err != nil; t, err = childIter() {
+        if err != nil {
+            return nil, err
+        }
+        tuples = append(tuples, t)
+    }
+
+    sort.Slice(tuples, func(i, j int) bool {
+        for k, expr := range o.orderBy {
+            eval_i, _ := expr.EvalExpr(tuples[i])
+            eval_j, _ := expr.EvalExpr(tuples[j])
+            switch expr.GetExprType().Ftype {
+            case IntType:
+                val_i := eval_i.(IntField).Value
+                val_j := eval_j.(IntField).Value
+                if val_i == val_j {continue}
+                if o.ascending[k] {
+                    return val_i < val_j
+                } else {
+                    return val_i > val_j
+                }
+            case StringType:
+                val_i := eval_i.(StringField).Value
+                val_j := eval_j.(StringField).Value
+                if val_i == val_j {continue}
+                if o.ascending[k] {
+                    return val_i < val_j
+                } else {
+                    return val_i > val_j
+                }
+            }
+        }
+        return true
+    })
+    ind := 0
+    return func() (*Tuple, error) {
+        if ind == len(tuples) {
+            return nil, err
+        }
+        ind++
+        return tuples[ind-1], nil
+    }, nil
 }
